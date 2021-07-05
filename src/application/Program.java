0a5f6620e692a5b830235d4775f0b9f9e12de9ef
Adson Santos <adsonconcecao@yahoo.com.br>
2021-07-04 23:23:45 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
@@ -39,7 +40,13 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+				
+			
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
